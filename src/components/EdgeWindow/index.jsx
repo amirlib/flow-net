7e4e28b63a18ddafc42b4cdc46700fa8808fc0c3
Amir <amir.lib@gmail.com>
2020-11-25 17:51:52 +0200

    converted EdgeWindow to class component

--- src/components/EdgeWindow/index.jsx
+++ src/components/EdgeWindow/index.jsx
@@ -4,10 +4,25 @@ import PropTypes from 'prop-types';
 import Button from '../Buttons/Button';
 import style from './window.module.scss';
 
-const EdgeWindow = (props) => {
-  const { addEdgeData, closeEdgeWindow, edgeWindowData } = props;
+export default class EdgeWindow extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const updateEdgeData = () => {
+    this.parseDisplayValue = this.parseDisplayValue.bind(this);
+    this.updateEdgeData = this.updateEdgeData.bind(this);
+  }
+
+  parseDisplayValue() {
+    const { edgeWindowData } = this.props;
+
+    if (edgeWindowData.display) return 'flex';
+
+    return 'none';
+  }
+
+  updateEdgeData() {
+    const { addEdgeData, edgeWindowData } = this.props;
+
     let capacity = parseInt(document.getElementById('capacity').value, 10);
     let flow = parseInt(document.getElementById('flow').value, 10);
 
@@ -15,71 +30,69 @@ const EdgeWindow = (props) => {
     if (Number.isNaN(flow)) flow = 0;
 
     addEdgeData(edgeWindowData.from, edgeWindowData.to, capacity, flow);
-  };
+  }
 
-  const parseDisplayValue = () => {
-    if (edgeWindowData.display) return 'flex';
+  render() {
+    const { closeEdgeWindow } = this.props;
 
-    return 'none';
-  };
+    return (
+      <div
+        className={style.root}
+        style={{ display: this.parseDisplayValue() }}
+      >
+        <div className={style.container}>
+          <p>
+            Please insert integer values of capacity and flow for the new edge.
+          </p>
+          <p>The default values are 1 for the capacity and 0 to flow.</p>
+          <p>Note that Edmonds Karp algorithm ignores flow values of the edges.</p>
 
-  return (
-    <div
-      className={style.root}
-      style={{ display: parseDisplayValue() }}
-    >
-      <div className={style.container}>
-        <p>
-          Please insert integer values of capacity and flow for the new edge.
-        </p>
-        <p>The default values are 1 for the capacity and 0 to flow.</p>
-        <p>Note that Edmonds Karp algorithm ignores flow values of the edges.</p>
+          <div className={style.form}>
+            <label
+              className={style.label}
+              htmlFor="capacity"
+            >
+              Capacity
+            </label>
+            <input
+              alt="capacity"
+              className={style.input}
+              id="capacity"
+              type="text"
+            />
+          </div>
 
-        <div className={style.form}>
-          <label
-            className={style.label}
-            htmlFor="capacity"
-          >
-            Capacity
-          </label>
-          <input
-            className={style.input}
-            type="number"
-            id="capacity"
-            alt="capacity"
-          />
-        </div>
+          <div className={style.form}>
+            <label
+              className={style.label}
+              htmlFor="flow"
+            >
+              Flow
+            </label>
+            <input
+              className={style.input}
+              type="number"
+              id="flow"
+              alt="flow"
+            />
+          </div>
 
-        <div className={style.form}>
-          <label
-            className={style.label}
-            htmlFor="flow"
-          >
-            Flow
-          </label>
-          <input
-            className={style.input}
-            type="number"
-            id="flow"
-            alt="flow"
-          />
-        </div>
+          <div className={style.form}>
+            <Button
+              func={this.updateEdgeData}
+              text="Create"
+            />
 
-        <div className={style.form}>
-          <Button
-            func={updateEdgeData}
-            text="Create"
-          />
-
-          <Button
-            func={closeEdgeWindow}
-            text="Cancel"
-          />
+            <Button
+              func={closeEdgeWindow}
+              text="Cancel"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 EdgeWindow.propTypes = {
   addEdgeData: PropTypes.func.isRequired,
@@ -90,5 +103,3 @@ EdgeWindow.propTypes = {
     to: PropTypes.number.isRequired,
   }).isRequired,
 };
-
-export default EdgeWindow;
